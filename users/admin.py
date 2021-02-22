a2b5e7f2b806197684cc24093537eb180e26366e
Jason <pajason2000@outlook.com>
2021-02-22 10:48:44 -0500

    OpticUsers added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import OpticUser
 
 # Register your models here.
+@admin.register(OpticUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id','email','first_name','last_name','optic','is_staff','is_active','is_superuser')
+    fieldsets = (
+        (None, {'fields': ([field.name for field in OpticUser._meta.get_fields() if (field.name!='logentry' and field.name!='id')])}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active','optic')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
--- users/managers.py
+++ users/managers.py
@@ -6,9 +6,29 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager donde el correo es el unico
     autenticador en vez del nombre de usuario
     """
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password):
         """
         Crea y guarda un usuario con el correo y contraseña dados.
         """
         if not email:
-            raise ValueError
+            raise ValueError(_('The email must be set'))
+        user = self.model(
+            email = self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Crea y guarda un superusuario con el correo y contraseña dados.
+        """
+        user = self.create_user(
+            email, password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
