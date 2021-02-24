6bde0921a9ea19e709a308db167b3956648afb57
carlos castillo blas <carlosCACB333@gmail.com>
2021-02-24 07:01:48 -0500

    optimizando el rest

--- users/rest.py
+++ users/rest.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from firebase_admin import auth
+
+from django.contrib.auth import login
+
 from .serializer import LoginSocialSerializer
 from .models import OpticUser
 
@@ -22,48 +25,38 @@ class GoogleLoginView(APIView):
 
         email = decode_token['email']
         name = decode_token['name']
-        avatar = decode_token['picture']
-        verified = decode_token['email_verified']
 
         print("========================")
-        print(decode_token)
-        for i,valor in decode_token.items():
-            print(i+"==="+str(valor))
 
-        # usuario, created = OpticUser.objects.get_or_create(
-        #     email=email,
-        #     defaults={
-        #         'full_name': name,
-        #         'email': email,
-        #         'is_active': True,
-        #     }
-        #
-        # )
-        #
-        # login(self.request, usuario)
-        # # creamos un token para administras nuestros usuarios.es opcional
-        # if created:
-        #     token = Token.objects.create(user=usuario)
-        # else:
-        #     token = Token.objects.get(user=usuario)
-        #
-        # user_get = {
-        #     'id': usuario.pk,
-        #     'email': usuario.email,
-        #     'full_name': usuario.full_name,
-        #     'genero': usuario.genero,
-        #     'date_birth': usuario.date_birth,
-        #     'city': usuario.city,
-        # }
-        # return Response(
-        #     {
-        #         'token': token.key,
-        #         'user': user_get,
-        #     }
-        # )
+        for i, valor in decode_token.items():
+            print(i + "===" + str(valor))
 
+        usuario, created = OpticUser.objects.get_or_create(
+            email=email,
+            defaults={
+                'full_name': name,
+                'is_active': True,
+                'is_staff': True,
+            }
+
+        )
+
+        login(self.request, usuario)
+        # creamos un token para administras nuestros usuarios.es opcional
+        if created:
+            token = Token.objects.create(user=usuario)
+        else:
+            token = Token.objects.get(user=usuario)
+
+        user_get = {
+            'id': usuario.pk,
+            'email': usuario.email,
+            'full_name': usuario.full_name,
+
+        }
         return Response(
             {
-                'user':email
+                'token': token.key,
+                'user': user_get,
             }
         )
