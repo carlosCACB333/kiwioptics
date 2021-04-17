c37fe27671aa462145e4ca0a7fe2620e7ba83142
Jason <pajason2000@outlook.com>
2021-04-16 19:53:33 -0500

    deplot

---
+++ myOptica/settings/prod.py
@@ -0,0 +1,29 @@
+from .base import *
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DB_URL')
+    )
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.RequestMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_URL = '/static/'
