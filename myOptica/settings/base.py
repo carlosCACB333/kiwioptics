958326916057755226013b13c4ab8204c12b891f
Jason <pajason2000@outlook.com>
2021-04-17 02:13:32 -0500

    ve

--- myOptica/settings/base.py
+++ myOptica/settings/base.py
@@ -31,17 +31,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
-
 ROOT_URLCONF = 'myOptica.urls'
 
 TEMPLATES = [
